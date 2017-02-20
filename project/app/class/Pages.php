41a18bc4f42057f6c6aa9be1b61ff7d2420111bd
klenzo <crea2luxe@gmail.com>
2017-02-20 18:15:57 +0100

    Config htaccess & Classes\Page

--- project/app/class/Pages.php
+++ project/app/class/Pages.php
@@ -22,4 +22,32 @@ class Pages
         }
         return $pageSlug;
     }
+
+    static public function getPost($item = false)
+    {
+        if( $item ){
+            if( isset( $_POST[$item] ) ){
+                return $_POST[$item];
+            }else {
+                return array();
+            }
+        }else{
+            return $_POST;
+        }
+    }
+
+    static public function getGet($item = false)
+    {
+        if( $item ){
+            if( isset( $_GET[$item] ) ){
+                return $_GET[$item];
+            }else {
+                return array();
+            }
+        }else{
+            return $_GET;
+        }
+    }
+
+
 }
