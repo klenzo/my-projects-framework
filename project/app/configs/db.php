b41ad92d7b8bb89ff9d630e8c22b404ddbd19179
klenzo <crea2luxe@gmail.com>
2017-04-06 21:51:51 +0200

    Db Config

---
+++ project/app/configs/db.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_NAME', 'project');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'admin');
+    define('DB_PREFIX', '64aAds_');
+    define('DB_CHARSET', 'UTF8');
